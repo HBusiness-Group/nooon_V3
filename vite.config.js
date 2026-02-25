756342ea26119b97354a32ddb64a45189394f686
HBusiness-Group <216771493+HBusiness-Group@users.noreply.github.com>
2026-02-25 15:54:18 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,7 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+export default defineConfig({
+  plugins: [react()],
+  base: "/nooon_V3/", // <- TEM que ser o nome exato do repositório
+});
