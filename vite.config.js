9c4f92b11ed7c15ddc13370fc4340958ed25c265
HBusiness-Group <216771493+HBusiness-Group@users.noreply.github.com>
2026-02-26 08:44:35 -0400

    Fix formatting in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // <- GitHub Pages: caminho relativo (evita branco)
+  base: "./"
 });
